d07af8a747b4e38f1b585719be30fa8e3d995a90
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-12-09 06:30:24 +0900

    引数、返却値、変数の型指定。strictモードについて

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Practice1 } from "./practice/Practice1";
+import { Practice2 } from "./practice/Practice2";
+import { Practice3 } from "./practice/Practice3";
+import { Practice4 } from "./practice/Practice4";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <Practice1 />
+      <Practice2 />
+      <Practice3 />
+      <Practice4 />
     </div>
   );
 }
